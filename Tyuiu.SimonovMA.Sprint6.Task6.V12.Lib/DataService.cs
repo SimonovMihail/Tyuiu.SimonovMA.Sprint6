ce29c0a9ea9eb37dcbb2c2ae279995050ea76007
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-12-09 11:19:41 +0500

    Task6 Fixed

--- Tyuiu.SimonovMA.Sprint6.Task6.V12.Lib/DataService.cs
+++ Tyuiu.SimonovMA.Sprint6.Task6.V12.Lib/DataService.cs
@@ -4,13 +4,13 @@ namespace Tyuiu.SimonovMA.Sprint6.Task6.V12.Lib
 {
     public class DataService : ISprint6Task6V12
     {
-        public string CollectTextFromFile(string str, string path)
+        public string CollectTextFromFile(string path)
         {
             var content = File.ReadAllText(path);
 
             var result = content
                     .Split(new[] { ' ', '\n', '\r', '\t', ',', '.', '!', '?', ';', ':', '(', ')', '[', ']', '{', '}' }, StringSplitOptions.RemoveEmptyEntries)
-                    .Where(word => word.Contains(str, StringComparison.OrdinalIgnoreCase))
+                    .Where(word => word.Contains('w', StringComparison.OrdinalIgnoreCase))
                     .Aggregate((current, next) => $"{current} {next}");
 
             return Convert.ToString(result);
