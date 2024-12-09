7964e9fe3c620007ab0c52ae478bd492eaf5f753
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-12-09 11:41:34 +0500

    Task6

--- Tyuiu.SimonovMA.Sprint6.Task6.V12/FormMain.Designer.cs
+++ Tyuiu.SimonovMA.Sprint6.Task6.V12/FormMain.Designer.cs
@@ -34,7 +34,14 @@
             buttonAbout_SMA = new Button();
             groupBoxTask_SMA = new GroupBox();
             textBoxTask_SMA = new TextBox();
+            textBoxIn_SMA = new TextBox();
+            textBoxOut_SMA = new TextBox();
+            groupBoxIn_SMA = new GroupBox();
+            groupBoxOut_SMA = new GroupBox();
+            openFileDialogTask_SMA = new OpenFileDialog();
             groupBoxTask_SMA.SuspendLayout();
+            groupBoxIn_SMA.SuspendLayout();
+            groupBoxOut_SMA.SuspendLayout();
             SuspendLayout();
             // 
             // buttonOpenFile_SMA
@@ -45,6 +52,7 @@
             buttonOpenFile_SMA.TabIndex = 0;
             buttonOpenFile_SMA.Text = "Файл";
             buttonOpenFile_SMA.UseVisualStyleBackColor = true;
+            buttonOpenFile_SMA.Click += buttonOpenFile_SMA_Click;
             // 
             // buttonRun_SMA
             // 
@@ -63,6 +71,7 @@
             buttonAbout_SMA.TabIndex = 2;
             buttonAbout_SMA.Text = "Справка";
             buttonAbout_SMA.UseVisualStyleBackColor = true;
+            buttonAbout_SMA.Click += buttonAbout_SMA_Click;
             // 
             // groupBoxTask_SMA
             // 
@@ -85,11 +94,55 @@
             textBoxTask_SMA.TabIndex = 0;
             textBoxTask_SMA.Text = resources.GetString("textBoxTask_SMA.Text");
             // 
+            // textBoxIn_SMA
+            // 
+            textBoxIn_SMA.Location = new Point(6, 22);
+            textBoxIn_SMA.Multiline = true;
+            textBoxIn_SMA.Name = "textBoxIn_SMA";
+            textBoxIn_SMA.ReadOnly = true;
+            textBoxIn_SMA.Size = new Size(366, 268);
+            textBoxIn_SMA.TabIndex = 4;
+            // 
+            // textBoxOut_SMA
+            // 
+            textBoxOut_SMA.Location = new Point(6, 22);
+            textBoxOut_SMA.Multiline = true;
+            textBoxOut_SMA.Name = "textBoxOut_SMA";
+            textBoxOut_SMA.ReadOnly = true;
+            textBoxOut_SMA.Size = new Size(368, 262);
+            textBoxOut_SMA.TabIndex = 5;
+            // 
+            // groupBoxIn_SMA
+            // 
+            groupBoxIn_SMA.Controls.Add(textBoxIn_SMA);
+            groupBoxIn_SMA.Location = new Point(18, 142);
+            groupBoxIn_SMA.Name = "groupBoxIn_SMA";
+            groupBoxIn_SMA.Size = new Size(378, 296);
+            groupBoxIn_SMA.TabIndex = 6;
+            groupBoxIn_SMA.TabStop = false;
+            groupBoxIn_SMA.Text = "Ввод";
+            // 
+            // groupBoxOut_SMA
+            // 
+            groupBoxOut_SMA.Controls.Add(textBoxOut_SMA);
+            groupBoxOut_SMA.Location = new Point(402, 142);
+            groupBoxOut_SMA.Name = "groupBoxOut_SMA";
+            groupBoxOut_SMA.Size = new Size(380, 290);
+            groupBoxOut_SMA.TabIndex = 0;
+            groupBoxOut_SMA.TabStop = false;
+            groupBoxOut_SMA.Text = "Вывод";
+            // 
+            // openFileDialogTask_SMA
+            // 
+            openFileDialogTask_SMA.FileName = "openFileDialog1";
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBoxOut_SMA);
+            Controls.Add(groupBoxIn_SMA);
             Controls.Add(groupBoxTask_SMA);
             Controls.Add(buttonAbout_SMA);
             Controls.Add(buttonRun_SMA);
@@ -101,6 +154,10 @@
             Load += Form1_Load;
             groupBoxTask_SMA.ResumeLayout(false);
             groupBoxTask_SMA.PerformLayout();
+            groupBoxIn_SMA.ResumeLayout(false);
+            groupBoxIn_SMA.PerformLayout();
+            groupBoxOut_SMA.ResumeLayout(false);
+            groupBoxOut_SMA.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -111,5 +168,10 @@
         private Button buttonAbout_SMA;
         private GroupBox groupBoxTask_SMA;
         private TextBox textBoxTask_SMA;
+        private TextBox textBoxIn_SMA;
+        private TextBox textBoxOut_SMA;
+        private GroupBox groupBoxIn_SMA;
+        private GroupBox groupBoxOut_SMA;
+        private OpenFileDialog openFileDialogTask_SMA;
     }
 }
--- Tyuiu.SimonovMA.Sprint6.Task6.V12/FormMain.cs
+++ Tyuiu.SimonovMA.Sprint6.Task6.V12/FormMain.cs
@@ -18,5 +18,15 @@ namespace Tyuiu.SimonovMA.Sprint6.Task6.V12
         {
 
         }
+
+        private void buttonAbout_SMA_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 6 выполнил студент группы РППб-24-1 Симонов Михаил Алексеевич", "Сообщение", MessageBoxButtons.OK);
+        }
+
+        private void buttonOpenFile_SMA_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
